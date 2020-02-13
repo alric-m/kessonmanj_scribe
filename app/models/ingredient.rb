b18d328bf0409e9836a2734a09d02b026f64540d
Matthias <matthias.alric@gmail.com>
2020-02-13 16:32:32 +0100

    add dependent destroy on join table ; clean recipe model

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,7 +1,8 @@
 class Ingredient < ApplicationRecord
-  has_many :recipes_ingredients
+
+  has_many :recipes_ingredients, dependent: :destroy
   has_many :recipes, through: :recipes_ingredients
 
   validates :name, presence: true, uniqueness: true
+
 end
-  
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,20 +2,8 @@ class Recipe < ApplicationRecord
 
   acts_as_taggable
 
-  has_many :recipes_ingredients
+  has_many :recipes_ingredients, dependent: :destroy
   has_many :ingredients, through: :recipes_ingredients
 
-  #validates :rate, presence: true
-  #validates :author_tip, presence: true
-  validates :budget, presence: true
-  #validates :prep_time, presence: true
-  validates :name, presence: true
-  validates :author, presence: true
-  #validates :difficulty, presence: true
-  #validates :people_quantity, presence: true
-  #validates :cook_time, presence: true
-  #validates :total_time, presence: true
-  #validates :image, presence: true
-  #validates :nb_comments, presence: true
+  validates :name, :author, presence: true
 end
-  
