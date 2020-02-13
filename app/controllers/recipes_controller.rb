051ab6b8e253f154261e40768be8e3742c72ec90
Matthias <matthias.alric@gmail.com>
2020-02-13 01:04:07 +0100

    order recipes by rating

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,6 +3,7 @@ class RecipesController < ApplicationController
   def index
     @recipes = SearchService.new(Recipe)
       .perform_query(params)
+      .order("rate DESC NULLS LAST")
       .paginate(page: params[:page], per_page: 12)
   end
 
